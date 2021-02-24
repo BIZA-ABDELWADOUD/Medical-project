816144eb5b69b6ef49cded40c8f7edf929e42ec3
BIZA-ABDELWADOUD <wadoud180@gmail.com>
2021-02-24 11:12:46 +0100

    other commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <!-- Default to the left -->
+    <strong>Copyright &copy; {{date('Y')}} <a href="https://adminlte.io">BIZA ABDELWADOUD</a>.</strong> All rights reserved.
+  </footer>
